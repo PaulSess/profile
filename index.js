eebcdbbe9408da5adb625e426a12cb74ee0db1c4
Paul Sessbruegger <paul.at.home@gmx.de>
2023-01-27 22:36:24 +0100

    mouse-click mouseover function included

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const school_project_drop = document.getElementById("schooldrop")
 const school_drop_icon = document.getElementById("school-drop-icon")
 const school_projects = document.getElementById("school-projects")
 
+
+// mouse hover effect
+const mouse1 = document.getElementById("mouse-click1")
+school_project_drop.addEventListener("mouseover", function(event) {
+  if (event.target.className === "sp") {
+    document.getElementById("mouse-click1").style.display = "block";
+  }
+});
+
+document.addEventListener("mouseout", function(event) {
+  if (event.target.className === "sp") {
+    document.getElementById("mouse-click1").style.display = "none";
+  }
+});
+
+
+
+
+
 school_project_drop.addEventListener("click", (event) => {
   console.log(event)
   // school_drop_icon.classList.remove("fa-solid", "fa-caret-right");
